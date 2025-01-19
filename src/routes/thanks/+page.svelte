3b7896a7dde9c0691cd96c470d118504673ed6d3
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2025-01-19 19:36:07 +0200

    ghfgh

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -14,12 +14,12 @@ import { goto } from '$app/navigation';
 </script>
 
 {#if success}
-	<div class="text-center">
+	<div class="text-center mt-20">
 		<h1 class="text-4xl font-bold text-green-600">תודה רבה!</h1>
 		<p class="text-lg mt-4">הזמנתך אושרה בהצלחה.</p>
 	</div>
 {:else}
-	<div class="text-center">
+	<div class="text-center mt-20">
 		<h1 class="text-4xl font-bold text-red-600">אופס!</h1>
 		<p class="text-lg mt-4">משהו השתבש. אנא נסה שוב.</p>
 	</div>
