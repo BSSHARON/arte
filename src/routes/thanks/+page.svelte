8e5cb521f7648d8a779051214a79b5724382448a
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2025-01-19 19:21:58 +0200

    thanks page

---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from "$app/state";
+import { onMount } from "svelte";
+
+    onMount(()=>{
+        const params = page.url.searchParams;
+        params.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
+    })
+    </script>
